40b79ca5e8ba6e7718ed7729f490b59e71b6a99c
saeed <saed.emad477@gmail.com>
2023-12-13 23:45:58 +0200

    msa msa

---
+++ src/main/java/com/mycompany/ma7fazty/Ma7fazty.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ma7fazty;
+
+/**
+ *
+ * @author Abdallah
+ */
+public class Ma7fazty {
+
+    public static void main(String[] args) {
+        new Sign_in().setVisible(true);
+    }
+}
